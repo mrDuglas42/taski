70ea85f4337cbd218da8ee09303a6f51f9dbcceb
mrduglas42 <kirmitius@yandex.ru>
2024-05-18 06:07:05 +0000

    Add workflow with PEP8 linting add setup.cfg

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from rest_framework import routers
-from django.urls import include, path
 from api import views
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
